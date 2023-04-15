329b6d4af23a45b513c60b726e838910a60ef222
Eduardo Ballico <ballico591@gmail.com>
2023-04-15 17:52:15 -0300

    some identation

--- Os.py
+++ Os.py
@@ -1,15 +1,15 @@
 from Process import Process
 
+
 class Os():
     def __init__(self, process):
         self.instrucoes = {
             "aritmetic": ["ADD", "SUB", "MULT", "DIV"],
             "memory": ["LOAD", "STORE"],
-            "jump": ["BRANY", "BRPOS", "BRZERO", "BRNEG"], 
+            "jump": ["BRANY", "BRPOS", "BRZERO", "BRNEG"],
             "system": ["SYSCALL"]
         }
         self.process: Process = process
-
 
     def aritmeticInstructions(self, op1: int, instruction: str, process: Process):
         if instruction == "ADD":
@@ -20,29 +20,31 @@ class Os():
             process.acc *= op1
         elif instruction == "DIV":
             process.acc /= op1
-            #LIDAR COM DIV POR 0
+            # LIDAR COM DIV POR 0
+
 
 def memoryInstructions(self, op1: str, instruction: str, process: Process):
-        if instruction == "LOAD":
-            process.acc = op1
-        elif instruction == "STORE":
-             process.data[op1] = process.acc
+    if instruction == "LOAD":
+        process.acc = op1
+    elif instruction == "STORE":
+        process.data[op1] = process.acc
 
 
 def memoryInstructions(self, instruction: str, process: Process, posLabel: int):
-        if instruction == "BRANY":
+    if instruction == "BRANY":
+        process.pc = posLabel
+    elif instruction == "BRPOS":
+        if process.acc > 0:
             process.pc = posLabel
-        elif instruction == "BRPOS":
-            if process.acc > 0:
-                process.pc = posLabel
-        elif instruction == "BRZERO":
-            if process.acc == 0:
-                process.pc = posLabel
-        
-        elif instruction == "BRNEG":
-            if process.acc < 0:
-                process.pc = posLabel
-        
+    elif instruction == "BRZERO":
+        if process.acc == 0:
+            process.pc = posLabel
+
+    elif instruction == "BRNEG":
+        if process.acc < 0:
+            process.pc = posLabel
+
+
 def memoryInstructions(self, process: Process, index: int):
     if index == 0:
         pass
--- Process.py
+++ Process.py
@@ -4,32 +4,31 @@ class Process():
         self.data = {}
         self.pc: int = 0
         self.acc: int = 0
-    
+
     def read_process(self, filename):
         isCode = False
         isData = False
         with open(filename, 'r') as file:
-              for l in file:
-                  if l[0:8] == '.enddata':
-                      isData = False         
-                  if l[0:8] == '.endcode':
-                      isCode = False
+            for l in file:
+                if l[0:8] == '.enddata':
+                    isData = False
+                if l[0:8] == '.endcode':
+                    isCode = False
 
-                  if isData:
-                      l_split = l.strip().split()
-                      self.data[l_split[0]] = l_split[1] 
-                  if isCode:
-                      l_split = l.strip().split()
-                      if l_split[0][len(l_split[0]) - 1] == ':':
-                          self.code.append(("label", l_split[0]))
-                      else:
-                          self.code.append((l_split[0], l_split[1]))
+                if isData:
+                    l_split = l.strip().split()
+                    self.data[l_split[0]] = l_split[1]
+                if isCode:
+                    l_split = l.strip().split()
+                    if l_split[0][len(l_split[0]) - 1] == ':':
+                        self.code.append(("label", l_split[0]))
+                    else:
+                        self.code.append((l_split[0], l_split[1]))
 
-                  if l[0:5] == '.code':
-                      isCode = True
-                  if l[0:5] == '.data':
-                      isData = True
-
+                if l[0:5] == '.code':
+                    isCode = True
+                if l[0:5] == '.data':
+                    isData = True
 
 
 process = Process()
@@ -38,4 +37,3 @@ process.read_process('ex_pgms_tp1/prog2.txt')
 
 # print(process.data)
 print(process.code)
-
